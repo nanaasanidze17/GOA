abee119b98752d833d4b23755e26b318cd7826af
nanaasanidze17 <asanidzenana10@gmail.com>
2024-05-19 14:31:29 +0400

    commit

---
+++ Level 03/Classwork/python.py
@@ -0,0 +1,18 @@
+
+# int variable
+number1 = 17
+number2 = 6
+print(17 + 6)
+# int + int =int
+#float variable
+number1 = 5.2
+number2 = 3.5
+
+print(5.2 + 3.5)
+#float + float = float
+
+bool1 = True
+bool2 = False
+
+print(int(bool1)+ int(bool2))
+
